5ea0c2bd365940243f6d508455eb77884f3379d1
Rex Aliis <77251557+RexAliis@users.noreply.github.com>
2023-02-06 20:45:24 -0300

    fix: :lock: non-null assertion

--- src/modules/say/index.ts
+++ src/modules/say/index.ts
@@ -1,10 +1,9 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import client from '@root/client';
 
 client.on('interactionCreate', interaction => {
 	if(!interaction.isChatInputCommand() || interaction.commandName !== 'say') return;
 
-	const text = interaction.options.getString('text')!;
+	const text = interaction.options.getString('text') ?? '';
 
 	if(text.includes('@everyone') || text.includes('@here') || /<@&(.*)>/.test(text)) {
 		interaction.reply('https://cdn.discordapp.com/attachments/852676149124792330/1072293491939487924/cooltext429078177054731.gif');
